6d268aa809a701b70e3333b2b3a2cae2f0f5a0a0
yashagrawal101 <58508158+yashagrawal101@users.noreply.github.com>
2021-09-02 15:15:12 +1000

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='myFlaskApp',
+      version='1.0',
+      description='myFlaskApp',
+      author='Prahlad Yeri',
+      author_email='prahladyeri@yahoo.com',
+      url='http://www.python.org/sigs/distutils-sig/',
+      install_requires=['Flask==0.10.1','SQLAlchemy==0.9.8'],
+     )
